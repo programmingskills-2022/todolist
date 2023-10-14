07f665de1ec6088ca9db0e7ec0a08615657e46d5
taraneh <taraneh.haeri@gmail.com>
2023-10-14 21:53:42 +0330

    first commit

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -77,24 +77,24 @@ WSGI_APPLICATION = 'todoapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default' : {
-        'ENGINE' : 'django.db.backends.postgresql',
-        'NAME' : 'railway',
-        'USER' : 'postgres',
-        'PASSWORD' : 'uChVQyiBOeZOTSlLql1f',
-        'HOST' : 'containers-us-west-196.railway.app',
-        'PORT' : '6625',    
-
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default' : {
+#         'ENGINE' : 'django.db.backends.postgresql',
+#         'NAME' : 'railway',
+#         'USER' : 'postgres',
+#         'PASSWORD' : 'uChVQyiBOeZOTSlLql1f',
+#         'HOST' : 'containers-us-west-196.railway.app',
+#         'PORT' : '6625',    
+
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
