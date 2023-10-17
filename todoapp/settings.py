37f2e9c7f2f46915b59fbdac046e2bd28149e001
taraneh <taraneh.haeri@gmail.com>
2023-10-17 08:53:16 +0330

    third commit

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -78,20 +78,20 @@ WSGI_APPLICATION = 'todoapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'data', 'db.sqlite3'),
-    }
-
-}
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3', 
+#         'NAME': os.path.join(BASE_DIR, 'data', 'db.sqlite3'),
+#     }
+
 # }
-# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3', 
+}
+}
 
 
 # Password validation
