8dc19800af1eb9c4c75376dbf00006d5e9ac3481
taraneh <taraneh.haeri@gmail.com>
2023-10-17 09:36:34 +0330

    third commit

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,6 +93,12 @@ DATABASES = {
 }
 }
 
+
+
+DATABASES['default'] = dj_database_url.config(
+    conn_max_age=600,
+    conn_health_checks=True,
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
